3e6d48a3c202ad877c262b050aef0e7453ddcf04
alek <beerwerd@gmail.com>
2023-01-18 14:43:50 +0200

    update postgres data

---
+++ postgres/dump.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30),
+  email VARCHAR(30)
+);
+
+INSERT INTO users ("name", "email") VALUES ('alex', 'beerwerd@gmail.com'), ('guest', 'some.user@gmail.com');
